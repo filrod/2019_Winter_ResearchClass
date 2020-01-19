61fc4f31fc6c3d0f5e65229fa5ec7a2b50e9019d
Filipe Rodrigues <filipe.rodrigues@mail.mcgill.ca>
2020-01-19 00:19:28 -0500

    First Vertical slice - Planet mesh and intro to noise
    Basic first vertical slice of planet demo done

--- Sandbox/Procedural_Planet_Generation/Assets/Scripts/Terrain_Face.cs
+++ Sandbox/Procedural_Planet_Generation/Assets/Scripts/Terrain_Face.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Terrain_Face
 {
+    // Contains information on how to modify meshes due to planet radious
+    ShapeGenerator shapeGenerator;
 
     Mesh mesh;
+
     /** Holds the level of detail */
     int resolution;
+
     /** Provides information about which way the Terrain_Face is facing */
     Vector3 localUp;
 
@@ -16,8 +20,9 @@ public class Terrain_Face
     Vector3 axisB;
 
     /** Conctructor for the Terrain_Face object */
-    public Terrain_Face(Mesh mesh, int resolution, Vector3 localUp)
+    public Terrain_Face(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
+        this.shapeGenerator = shapeGenerator;
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -25,56 +30,60 @@ public class Terrain_Face
         // Set axis vectors
         axisA = new Vector3(localUp.y, localUp.z, localUp.x);
         axisB = Vector3.Cross(localUp, axisA);
+    }
 
-        public void constructMesh()
+    public void constructMesh()
+    {
+        // hold vertices
+        Vector3[] vertices = new Vector3[resolution * resolution];
+
+        int amountOfVerticesPerSide = resolution - 1;
+        int trianglesInASquare = 2;
+        int verticesInATriangle = 3;
+
+        int[] triangles = new int[
+            amountOfVerticesPerSide * amountOfVerticesPerSide
+            * trianglesInASquare * verticesInATriangle
+            ];
+
+        int triangleIndex = 0;
+        for (int y = 0; y < resolution; y++)
         {
-            // hold vertices
-            Vector3[] vertices = new Vector3[resolution * resolution];
-            int amountOfVerticesPerSide = resolution - 1;
-            int trianglesInASquare = 2;
-            int verticesInATriangle = 3;
-            int[] triangles = new int[
-                amountOfVerticesPerSide * amountOfVerticesPerSide 
-                * trianglesInASquare * verticesInATriangle
-                ];
-
-            // Nested for loops to see what fraction of the mesh we have covered. End is (1, 1)
-            int i = 0;
-            int triangleIndex = 0;
-            for (int y = 0; y < resolution; y++)
+            for (int x = 0; x < resolution; x++)
             {
-                for (int x = 0; x < resolution; x++)
+                int i = x + y * resolution;
+                Vector2 meshCompletion = new Vector2(x, y) / (resolution - 1);
+
+                // Where the vertex is along the face
+                Vector3 pointOnUnitCube = localUp + (meshCompletion.x - 0.5f) * 2 * axisA + (meshCompletion.y - 0.5f) * 2 * axisB;
+
+                // Normalize the unit cube
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+
+                // Calculate index to add to vertices array
+                vertices[i] = shapeGenerator.CalculatePointOnPlanet(pointOnUnitSphere);
+
+                if (x != resolution - 1 && y != resolution - 1)
                 {
-                    Vector2 meshCompletion = new Vector2(x, y) / (resolution - 1);
-                    // Where the vertex is along the face
-                    Vector3 pointOnUnitCube = localUp + (meshCompletion.x - 0.5f) * 2 * axisA + (meshCompletion.y - 0.5f) * 2 * axisB;
-                    // Calculate index to add to vertices array
-                    vertices[i] = pointOnUnitCube;
-                    i++;
+                    // Lower Left Hand Triangle of a quad
+                    triangles[triangleIndex] = i;
+                    triangles[triangleIndex + 1] = i + resolution + 1;
+                    triangles[triangleIndex + 2] = i + resolution;
 
-                    if (x != resolution - 1 && y != resolution - 1)
-                    {
-                        // Lower Left Hand Triangle of a quad
-                        triangles[triangleIndex] = i;
-                        triangles[triangleIndex + 1] = i + resolution+1;
-                        triangles[triangleIndex + 2] = i + resolution;
+                    // Upper Right hand Triangle of a quad
+                    triangles[triangleIndex + 3] = i;
+                    triangles[triangleIndex + 4] = i + 1;
+                    triangles[triangleIndex + 5] = i + resolution + 1;
 
-                        // Upper Right hand Triangle of a quad
-                        triangles[triangleIndex + 3] = i;
-                        triangles[triangleIndex + 4] = i + 1;
-                        triangles[triangleIndex + 5] = i + resolution+1;
-
-                        // We moved along by 6 entries in triangles so increment by 6
-                        triangleIndex += 6;
-                    }
+                    // We moved along by 6 entries in triangles so increment by 6
+                    triangleIndex += 6;
                 }
             }
-
-            // Assign to mesh
-            mesh.Clear();  // in case we lower resolution to avoid indexOutOfBounds error
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-            mesh.RecalculateNormals();
         }
+        // Assign vertices and triangles to mesh
+        mesh.Clear();  // in case we lower resolution to avoid indexOutOfBounds error
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
