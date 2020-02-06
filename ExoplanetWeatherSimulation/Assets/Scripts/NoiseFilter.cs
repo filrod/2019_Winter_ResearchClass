6bb1d4d0de42b9c9efcc3af1c194f7064af57e26
Filipe Rodrigues <filipe.rodrigues@mail.mcgill.ca>
2020-02-06 11:18:07 -0500

    Ported sandbox project to stable unity version
    Relates to:
    Version control issues when porting to another PC #2

---
+++ ExoplanetWeatherSimulation/Assets/Scripts/NoiseFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseFilter
+{
+    NoiseSettings settings;
+    Noise noise = new Noise();
+
+    public NoiseFilter(NoiseSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    // Point is where we evaluate and apply processing
+    public float Evaluate(Vector3 point)
+    {
+        // noise.Evaluate(point) is in [-1, 1] so ad 1 for [0, 1]
+        float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * 0.5f;
+        return noiseValue * settings.strength;
+    }
+}
---
+++ ExoplanetWeatherSimulation/Assets/Scripts/NoiseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    // Strength of the noise
+    public float strength = 1;
+    // Roughness of the noise
+    public float roughness = 1;
+    // Centre of noise
+    public Vector3 center;
+
+}
